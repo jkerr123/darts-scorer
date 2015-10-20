999884ef612367b46c2a079951c9ce807074321b
Jamie Kerr <j.w.kerr@dundee.ac.uk>
2015-10-20 19:19:41 +0100

    Heroku Deployment Test

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
+import logging
 from flask import Flask, render_template
 import pymongo
-
+import sys
 
 app = Flask(__name__)
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 __author__ = 'jamie'
 
