c747922118bc55a2c4f3073cc6832d47b2ba9c2a
Jamie Kerr <j.w.kerr@dundee.ac.uk>
2015-11-12 15:48:44 +0000

    Changed from uwsgi to gunicorn for sockets to work properly

--- src/app.py
+++ src/app.py
@@ -22,13 +22,9 @@ socketio = SocketIO(app)
 def ssl_required(fn):
     @wraps(fn)
     def decorated_view(*args, **kwargs):
-        if current_app.config.get("SSL"):
-            if request.is_secure:
-                return fn(*args, **kwargs)
-            else:
+
                 return redirect(request.url.replace("http://", "https://"))
 
-        return fn(*args, **kwargs)
 
     return decorated_view
 
